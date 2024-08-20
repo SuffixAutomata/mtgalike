2a6271946770a9a618442c890ce1f11b05936077
Terezi/Darcy <2464973-terezi@users.noreply.gitlab.com>
2024-08-20 18:42:02 +0800

    seocnd com

--- sim.py
+++ sim.py
@@ -1,9 +1,14 @@
 class PlayerState():
-  def __init__(self):
+  def __init__(self, name):
     self.drawDeck : list[Card] = []
     self.discardDeck : list[Card] = []
     self.hand : list[Card] = []
-    self.hand : list[Card] = []
+    self.board : list[Card] = []
+    self.name = name
+  def prompt(self, v: str):
+    return input(f"[{self.name}] {v} ")
+  def output(self, v: str):
+    print(f"[{self.name}] {v}")
 
 class Card:
   def __init__(self):
